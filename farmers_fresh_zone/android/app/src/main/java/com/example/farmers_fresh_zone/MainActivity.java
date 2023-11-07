94d50a514b0a1d265d768f916889e0a92bac7be1
Safal <safalsaleem11@gmail.com>
2023-11-07 22:38:01 +0530

    Farmers fresh zone

---
+++ farmers_fresh_zone/android/app/src/main/java/com/example/farmers_fresh_zone/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.farmers_fresh_zone;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ gridview_stack/android/app/src/main/java/com/example/gridview_stack/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.gridview_stack;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
